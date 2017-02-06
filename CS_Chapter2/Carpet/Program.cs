1ff75c70cf3c0293dfa09927ae2c5aba4b305a09
ImBadWithUsernames <antoniogonz2001@gmail.com>
2017-02-06 14:19:21 -0500

    Did Room.cs and started Carpet.cs

--- CS_Chapter2/Carpet/Program.cs
+++ CS_Chapter2/Carpet/Program.cs
@@ -9,6 +9,11 @@ namespace Carpet
     {
         static void Main(string[] args)
         {
+            int length = 15;
+            int width = 25;
+            int area = length * width;
+            int cost = area * 2;
+            Console.WriteLine("The cost of the carpet per square foot is {0}, and the floor is about {1} square feet.", $2, area);
         }
     }
 }
